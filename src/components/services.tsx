27abf72c81eeef4297cfedc8643ae7f0d686fd49
Leonardo <leonardomenezes47@outlook.com>
2025-01-23 17:42:39 -0300

    first commiddddsssstsss

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,7 +1,7 @@
 export function Services() {
   return(
-    <div className="mt-40 space-y-6 max-lg:p-6">
-      <h2 className="px-12 text-4xl text-green-500 italic font-semibold max-lg:text-3xl">
+    <div className="mt-40 space-y-6">
+      <h2 className="px-12 text-4xl text-green-500 italic font-semibold max-lg:text-2xl max-lg:px-6">
         Serviços que o SIAMT oferece aos seus associados
       </h2>
 
