c7c33bad403a4686168c29c33b9a314511802696
Leonardo <leonardomenezes47@outlook.com>
2025-02-27 21:20:06 -0400

    add sliders

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,6 +9,7 @@ import { Status } from "@/components/status"
 import { Associates } from "@/components/associates"
 import { Join } from "@/components/join"
 import { Curriculum } from "@/components/curriculum"
+import { Slider } from "@/components/slider"
 
 export function Home() {
   return(
@@ -16,9 +17,7 @@ export function Home() {
       <Header/>
 
       <main className="flex flex-col">
-        <div className="bg-home w-full h-[800px] bg-cover bg-center flex items-start justify-center">
-          <h1 className="text-7xl text-slate-50 font-bold border-b-4 py-5 uppercase italic mt-60 max-lg:text-3xl">Bem-vindos ao SIAMT</h1>
-        </div>
+        <Slider/>
 
         <News/>
 
